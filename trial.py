7a831a53f598ffef197d3f9c5d701b911d9eaed0
michal <michallis95@vp.pl>
2018-04-26 11:53:35 +0200

    Overfiiting data

---
+++ trial.py
@@ -0,0 +1,15 @@
+class Location(object):
+    def __init__(self, x, y):
+        """x and y are floats"""
+        self.x = x
+        self.y = y
+
+    def move(self, deltaX, deltaY):
+        return Location(self.x + deltaX, self.y + deltaY)
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
